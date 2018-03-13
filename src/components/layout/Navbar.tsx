2f7438f3cec20e8cb143b1096017e18c843441fd
Devid Farinelli <devid.farinelli@gmail.com>
2018-03-13 21:24:04 +0100

    New Navbar container

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,22 +2,17 @@ import * as React from 'react'
 import { css, StyleSheet } from 'aphrodite'
 
 const styles = StyleSheet.create({
-    expand: {
-        flex: 1
-    },
-    logo: {
-        width: '120px',
-        height: '31px',
-        background: 'rgba(255,255,255,.9)',
-        margin: '16px 24px 16px 0',
-        float: 'left'
+    container: {
+        position: 'fixed',
+        width: '392px',
+        height: '48px',
+        background: 'rgba( 255, 255, 255, .9 )',
+        margin: '8px 0px 8px 8px',
+        float: 'left',
+        zIndex: 100
     }
 })
 
-const Navbar = () => (
-    <div>
-        <div className={css(styles.logo)} />
-    </div>
-)
+const Navbar = () => <div className={css(styles.container)}>Ciao</div>
 
 export default Navbar
