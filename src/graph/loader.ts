18f8d6045cfdfd66600dd6cb259e0efc3b6ddda9
Devid Farinelli <devid.farinelli@gmail.com>
2018-03-14 17:08:42 +0100

    Refactored xhr requests

--- src/graph/loader.ts
+++ src/graph/loader.ts
@@ -1,38 +1,6 @@
 import * as asyncFor from 'rafor'
 import config from '../config'
-
-function getBinary(url: string) {
-    return new Promise((resolve, reject) => {
-        const req = new XMLHttpRequest()
-        req.onload = (ev: Event) => {
-            if (req.status == 200) {
-                var buffer = new Int32Array(req.response)
-                resolve(buffer)
-            } else {
-                reject('Error fetching: ' + url)
-            }
-        }
-        req.open('GET', url, true)
-        req.responseType = 'arraybuffer'
-        req.send()
-    })
-}
-
-function getJson(url: string) {
-    return new Promise((resolve, reject) => {
-        const req = new XMLHttpRequest()
-        req.onload = (ev: Event) => {
-            if (req.status == 200) {
-                resolve(req.response)
-            } else {
-                reject('Error fetching: ' + url)
-            }
-        }
-        req.open('GET', url, true)
-        req.responseType = 'json'
-        req.send()
-    })
-}
+import { getBinary, getJson } from '../utils/xhr'
 
 const loadGraph = (name: string) => {
     // TODO Progress
